03bcce8703bd319cfdd83d01db5da3161f48fdef
marykeje <marykbisong@gmail.com>
2025-09-20 16:45:28 +0100

    making changes on the calculator

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-"use script"
+"use script";
 
-const result = document.getElementById('result')
-const buttons = document.querySelectorAll('.symbol')
+// const result = document.getElementById('result')
+// initialize the input to 0
+// variable for the operator symbols
 
-let add = 0
+// selecting all number buttons
+// const buttons = document.querySelectorAll('.symbol')
+
+// let add = 0;
+
+// for the form implementation
+// initializing the inputs value
+function calculate() {
+  let figure1 = document.getElementById("num1").value.Number;
+  let figure2 = document.getElementById("num2").value;
+
+  // for the operator
+  if (operator === add) {
+    result = figure1 + figure2;
+  } else if (operator === subtract) {
+    result = figure1 - figure2;
+  } else if (operator === multiply) {
+    result = figure1 * figure2;
+  } else if (operator === divide) {
+    result = figure1 / figure2;
+  }
+}
+
+
+let operate = 0
+
+function add() {
+   operate = 
+}
+
+// function add() {
+//   result = result + 1;
+//   submitEl.innerText = result;
+// }
+
+// for the submit button
+document.getElementById("demo").innerText = result;
